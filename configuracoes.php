5e43d122c7d2fa3b163d108356b97a79d2ad79d0
Eduardo <eduardo@redeserveloja.com>
2017-12-15 17:50:46 -0300

    Aba cartões

--- configuracoes.php
+++ configuracoes.php
@@ -38,54 +38,107 @@
     echo $funcoes::save_configuracoes($_POST["apl_nome"], $_POST["apl_token"], $_POST["apl_prefixo"], $_POST["apl_email"], $_POST["apl_ambiente"], $_POST["apl_id"]);
   } ?>
 
+  <?php // post cartões
+  if (isset($_POST["salvar_cartoes"])) {
+    // executa
+    echo $funcoes::insert_cartoes($_POST["posicao"], $_POST["car_cod"], $_POST["car_bandeira"], $_POST["car_parcelas"]);
+  } ?>
+
   <h1>Pagamentos Serveloja</h1>
   <h2>
     Caso você já seja cliente Serveloja, informe <b>Nome</b> e <b>Token</b> para validar o uso desta aplicação.
     Caso não, cadastra-se antes de continuar.
   </h2>
-  <h3>Configurações da aplicação</h3>
-  <p>Todos os campos marcados com <strong>(*)</strong> são de preenchimento obrigatório.</p>
-  <form name="configuracoes" method="post" action="">
-    <div class="tituloInput">Nome da Aplicação (*)</div>
-    <input type="text" class="input" name="apl_nome" value="<?php echo $apl_nome; ?>" maxlength="30" placeholder="Informe aqui, o nome da aplicação" />
-    <br />
-    <div class="tituloInput">Token da Aplicação (*)</div>
-    <input type="text" class="input" name="apl_token" value="<?php echo $apl_token; ?>" maxlength="30" placeholder="Informe aqui, o token da aplicação" />
-    <br />
-    <div class="tituloInput">Prefixo das transações</div>
-    <input type="text" class="input" name="apl_prefixo" value="<?php echo $apl_prefixo; ?>" placeholder="Informe aqui, o prefixo para idetificar as transações realizadas" />
-    <br />
-    <div class="tituloInput">Informe um e-mail para receber notificações sobre compras realizadas em seu site/loja</div>
-    <input type="text" class="input" name="apl_email" value="<?php echo $apl_email; ?>" placeholder="Informe aqui, o e-mail para contato" />
-    <br />
-    <div class="tituloInput">O que você pretende fazer com esta aplicação? (*)</div>
-    <select class="select" name="apl_ambiente">
-        <option value="0" <?php if ($apl_ambiente == "0") { echo 'selected="selected"'; } ?>>Apenas um teste, estou verificando o funcionamento</option>
-        <option value="1" <?php if ($apl_ambiente == "1") { echo 'selected="selected"'; } ?>>Vou utilizar em minha loja/site para receber pagamentos</option>
-    </select>
-    <br />
-    <input type="hidden" name="apl_id" value="<?php echo $apl_id; ?>" />
-    <input type="submit" class="submit" name="salvar_config" value="Salvar" name="salvar" />
-  </form>
 
-  <!-- dados dos cartões -->
-  <?php if ($dados != "0") { ?>
+  <!-- botões de controle dos paineis -->
+  <div class="botao-ativo" id="botao-a" onclick='controle_exibicao("aplicacao", "cartoes", "botao-a", "botao-b")'>
+      Configuração da aplicação
+  </div>
 
-    <div class="clear"></div>
-    <h3 style="margin-top: 30px;">Formas de recebimento</h3>
-    <p>
-      Abaixo, as bandeiras dos cartões que você pode utilizar para receber seus pagamentos.
-      Para utilizar, marque a opção <b>"Receber"</b> e informe em até quantas vezes as compras podem ser divididas no momento da compra.
-    </p>
+  <div class="botao-inativo" id="botao-b" onclick='controle_exibicao("cartoes", "aplicacao", "botao-b", "botao-a")'>
+      Cartões de crédito
+  </div>
 
-    <form method="post" name="cartoes" action="">
-      <div class="cartao"></div>
-      <div class="cartao"></div>
-      <div class="cartao"></div>
-      <div class="clear"></div>
+  <div class="clear"></div>
+
+  <!-- painel para cadastro de dados da aplicação -->
+  <div id="aplicacao" class="painel">
+    <h3>Configurações da aplicação</h3>
+    <p>Todos os campos marcados com <strong>(*)</strong> são de preenchimento obrigatório.</p>
+    <form name="configuracoes" method="post" action="">
+      <div class="tituloInput">Nome da Aplicação (*)</div>
+      <input type="text" class="input" name="apl_nome" value="<?php echo $apl_nome; ?>" maxlength="30" placeholder="Informe aqui, o nome da aplicação" />
+      <br />
+      <div class="tituloInput">Token da Aplicação (*)</div>
+      <input type="text" class="input" name="apl_token" value="<?php echo $apl_token; ?>" maxlength="30" placeholder="Informe aqui, o token da aplicação" />
+      <br />
+      <div class="tituloInput">Prefixo das transações</div>
+      <input type="text" class="input" name="apl_prefixo" value="<?php echo $apl_prefixo; ?>" placeholder="Informe aqui, o prefixo para idetificar as transações realizadas" />
+      <br />
+      <div class="tituloInput">Informe um e-mail para receber notificações sobre compras realizadas em seu site/loja</div>
+      <input type="text" class="input" name="apl_email" value="<?php echo $apl_email; ?>" placeholder="Informe aqui, o e-mail para contato" />
+      <br />
+      <div class="tituloInput">O que você pretende fazer com esta aplicação? (*)</div>
+      <select class="select" name="apl_ambiente">
+          <option value="0" <?php if ($apl_ambiente == "0") { echo 'selected="selected"'; } ?>>Apenas um teste, estou verificando o funcionamento</option>
+          <option value="1" <?php if ($apl_ambiente == "1") { echo 'selected="selected"'; } ?>>Vou utilizar em minha loja/site para receber pagamentos</option>
+      </select>
+      <br />
+      <input type="hidden" name="apl_id" value="<?php echo $apl_id; ?>" />
       <input type="submit" class="submit" name="salvar_config" value="Salvar" name="salvar" />
     </form>
+    <div class="clear"></div>
+  </div>
 
-  <?php } ?>
+  <!-- painel para salvar dados dos cartões -->
+  <div id="cartoes" style="display:none;" class="painel">
+
+    <h3>Cartões utilizados para recebimento</h3>
+    <?php if ($dados != "0") {
+    $dados_cartoes = $funcoes::cartoes();
+    $cod_array = array($dados_cartoes->car_cod);
+    print_r($dados_cartoes);
+    print_r($cod_array); ?>
+      <p>
+        Abaixo, as bandeiras dos cartões que você pode utilizar para receber seus pagamentos.
+        Para utilizar, marque a opção <b>"Receber"</b> e informe em até quantas vezes as compras podem ser divididas no momento da compra.
+      </p>
+      <?php // dados para montar form
+      $cartoes = $funcoes::lista_cartoes();
+      $form = '';
+      for ($i = 0; $i < count($cartoes); $i++) {
+        if (in_array($cartoes[$i]["cod"], array($dados_cartoes->car_cod))) {
+          echo "tem";
+        } else {
+          echo "não tem";
+        }
+        $form .= '<div class="cartao">';
+        $form .= '<div class="img_cartao">';
+          $form .= '<img src="' . PASTA_PLUGIN . 'images/' . $cartoes[$i]["bandeira"] . '.png" title="' . $cartoes[$i]["nome"] .'" alt="' . $cartoes[$i]["baneira"] . '" />';
+        $form .= '</div>';
+        $form .= '<input type="checkbox" name="posicao[]" value="' . $i .'" /><b> Utilizar ' . $cartoes[$i]["nome"] . '</b><br />';
+        $form .= '<input type="hidden" name="car_bandeira[]" value="' . $cartoes[$i]["bandeira"] . '" />';
+        $form .= '<input type="hidden" name="car_cod[]" value="' . $cartoes[$i]["cod"] . '" />';
+        $form .= '<select name="car_parcelas[]" class="select">';
+        for ($j = 1; $j <= 12; $j++) {
+          if ($j == 1) {
+            $form .= '<option value="' . $j . '">Apenas uma parcela</option>';
+          } else {
+            $form .= '<option value="' . $j . '">' . $j . ' parcelas</option>';
+          }
+        }
+        $form .= '</select>';
+        $form .= '</div>';
+      } ?>
+     <form method="post" name="cartoes" action="">
+        <?php echo $form; ?>
+        <div class="clear"></div>
+        <input type="submit" class="submit" name="salvar_cartoes" value="Salvar" name="salvar" />
+      </form>
+    <?php } else { ?>
+      <div class="alerta">Antes de selecionar as bandeiras dos cartões, você precisa informar os dados da aplicação</div>
+    <?php } ?>
+
+  </div>
 
 <?php } ?>
--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
     }
 
     // métodos
-    private function metodos_acesso($url, $method, $param) {
+    private function metodos_acesso_api($url, $method, $param) {
       if ($method == "get") {
         /* em caso do método GET, os parâmetros serão adicionados na URL, como padrão */
         $con = curl_init(Serveloja_functions::servidor() . $url . "?" . $param);
@@ -16,11 +16,11 @@
             'Content-Type: application/json'
           )
         );
-      } else {
+      } else if ($method == "post") {
         /* em caso de POST, os parãmetros serão adicionados a um array, ex: array("id" => "$id", "symbol" => "$symbol"); */
         $con = curl_init(Serveloja_functions::servidor() . $url . "?" . $param);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($con, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($con, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($con, CURLOPT_HTTPHEADER, array(
             'Content-Type: application/json',
             'Content-Length: ' . strlen(json_encode($param))
@@ -37,7 +37,7 @@
 
     // verifica se token informado por usuário é válido
     private function valida_token($url, $method, $param) {
-        return Serveloja_functions::metodos_acesso($url, $method, $param);
+        return Serveloja_functions::metodos_acesso_api($url, $method, $param);
     }
 
     // fecha div via javascript após alguns segundos
@@ -63,7 +63,11 @@
           ),
           array('%s', '%s', '%s', '%s', '%s')
       );
-      return Serveloja_functions::div_resposta("fecha_mensagem", "sucesso", "Os dados foram adicionados com sucesso");
+      if ($wpdb->last_error) {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "erro", "Ocorreu um erro: " . $wpdb->last_error);
+      } else {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "sucesso", "Os dados foram adicionados com sucesso");
+      }
     }
 
     private function update_aplicacao($apl_nome, $apl_token, $apl_prefixo, $apl_email, $apl_ambiente, $apl_id) {
@@ -80,9 +84,14 @@
           array('%s', '%s', '%s', '%s', '%s'),
           array('%s')
       );
-      return Serveloja_functions::div_resposta("fecha_mensagem", "sucesso", "Os dados foram modificados com sucesso");
+      if ($wpdb->last_error) {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "erro", "Ocorreu um erro: " . $wpdb->last_error);
+      } else {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "sucesso", "Os dados foram modificados com sucesso");
+      }
     }
 
+    // salva dados aplicação
     public function save_configuracoes($apl_nome, $apl_token, $apl_prefixo, $apl_email, $apl_ambiente, $apl_id) {
       if ($apl_nome == "" || $apl_token == "" || $apl_ambiente == "") {
         return Serveloja_functions::div_resposta("fecha_mensagem", "erro", "Os campos marcados com (*) devem ser preencidos");
@@ -95,11 +104,59 @@
       }
     }
 
+    // lista dados da aplicação
     public function aplicacao() {
       global $wpdb;
       $rows = $wpdb->get_results("SELECT apl_id, apl_nome, apl_token, apl_prefixo, apl_email, apl_ambiente FROM " . $wpdb->prefix . "aplicacao ORDER BY apl_id DESC LIMIT 1");
-      if (count($rows) == 0) {
-        return "0";
+      if ($wpdb->last_error) {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "erro", "Ocorreu um erro: " . $wpdb->last_error);
+      } else {
+        if (count($rows) == 0) {
+          return "0";
+        } else {
+          return $rows;
+        }
+      }
+    }
+
+    // cartões
+    public function lista_cartoes() {
+      return array(
+        array("cod" => "01", "nome" => "American Express", "bandeira" => "american", "parcelas" => 12),
+        array("cod" => "02", "nome" => "Diners Club", "bandeira" => "diners", "parcelas" => 12),
+        array("cod" => "03", "nome" => "Elo", "bandeira" => "elo", "parcelas" => 12),
+        array("cod" => "04", "nome" => "Hiper", "bandeira" => "hiper", "parcelas" => 12),
+        array("cod" => "06", "nome" => "Mastercard", "bandeira" => "mastercard", "parcelas" => 12),
+        array("cod" => "07", "nome" => "Visa", "bandeira" => "visa", "parcelas" => 12)
+      );
+    }
+
+    public function insert_cartoes($posicao, $car_cod, $car_bandeira, $car_parcelas) {
+      global $wpdb;
+      $wpdb->query("TRUNCATE TABLE " . $wpdb->prefix . "cartoes");
+      for ($i = 0; $i < count($posicao); $i++) {
+        $pos = $posicao[$i];
+        $wpdb->insert(
+            $wpdb->prefix . "cartoes",
+            array('car_cod' => $car_cod[$pos],
+                  'car_bandeira' => $car_bandeira[$pos],
+                  'car_parcelas' => $car_parcelas[$pos]
+            ),
+            array('%s', '%s', '%s')
+        );
+      }
+      if ($wpdb->last_error) {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "erro", "Ocorreu um erro: " . $wpdb->last_error);
+      } else {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "sucesso", "Os dados foram adicionados com sucesso");
+      }
+    }
+
+    public function cartoes() {
+      global $wpdb;
+      $rows = $wpdb->get_results("SELECT car_cod, car_bandeira, car_parcelas FROM " . $wpdb->prefix . "cartoes");
+      if ($wpdb->last_error) {
+        return Serveloja_functions::div_resposta("fecha_mensagem", "erro", "Ocorreu um erro: " . $wpdb->last_error);
       } else {
         return $rows;
       }
